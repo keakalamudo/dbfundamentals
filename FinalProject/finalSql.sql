dbbee4599336d0e6ca372fc076d5c74cc508cc8c
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-04-30 11:51:34 -0700

    Test

--- FinalProject/finalSql.sql
+++ FinalProject/finalSql.sql
@@ -19,15 +19,15 @@ It is used in the flight table and in the airportairline table
 */
 /* Create the Airport Table */
 CREATE TABLE Airline (
-	AirlineIcaoCode 			VARCHAR(256) 		NOT NULL,	/* PK */
-	AirlineName					VARCHAR(256),
-	AirlineHeadquarters			VARCHAR(256)
+	IcaoCode 				VARCHAR(256) 		NOT NULL,	/* PK */
+	Name					VARCHAR(256),
+	Headquarters			VARCHAR(256)
 ); /* End of Creation of Airline Table */
 
 /* Set up the Primary Key */
 ALTER TABLE Airline
 ADD CONSTRAINT AirlinePrimaryKey
-PRIMARY KEY (AirlineIcaoCode);
+PRIMARY KEY (IcaoCode);
 
 /*******************************************************************************
 Airport 
@@ -36,15 +36,15 @@ This table models an airport. Flights arrive and depart from an airport. There
 are multiple airlines at each airport.
 */
 CREATE TABLE Airport (
-	AirportFaaCode 				VARCHAR(256) 		NOT NULL,	/* PK */
-	AirpotName					VARCHAR(256),
-	AirportLocation				VARCHAR(256)
+	FaaCode 				VARCHAR(16) 		NOT NULL,	/* PK */
+	Name					VARCHAR(256),
+	Location				VARCHAR(256)
 ); /* End of Creation of Airport Table */
 
 /* Set up the Primary Key */
 ALTER TABLE Airport
 ADD CONSTRAINT AirportPrimaryKey
-PRIMARY KEY (AirportFaaCode);
+PRIMARY KEY (FaaCode);
 
 
 /*******************************************************************************
@@ -53,38 +53,65 @@ AirportAirline
 This table models an airline's presence at an airport. 
 */
 create table AirportAirline (
-	AirportAirlineTerminal 		VARCHAR(256) 		NOT NULL,	/* PK */
-	AirlineIcaoCode 			VARCHAR(256) 		NOT NULL,	/* FK(Airline) -> PK */
-	AirportFaaCode 				VARCHAR(256) 		NOT NULL,	/* FK(Airport) -> PK */
+	Terminal 				VARCHAR(256) 		NOT NULL,	/* PK */
+	IcaoCode 				VARCHAR(256) 		NOT NULL,	/* FK(Airline) -> PK */
+	FaaCode 				VARCHAR(256) 		NOT NULL,	/* FK(Airport) -> PK */
 
 ); /* End of Creation of Name Table */
 
-/* Set up the Primary Keys */
-ALTER TABLE AirportAirline
-ADD CONSTRAINT AirportPrimaryKey
-PRIMARY KEY (AirlineIcaoCode, AirportFaaCode);
+
 
 /* Set up the AirlineIcaoCode Foreign Key */
 ALTER TABLE AirportAirline
 ADD CONSTRAINT  AirlineIcaoCodeForeignKey
-FOREIGN KEY (AirlineIcaoCode)
-REFERENCES Airline (AirlineIcaoCode);
+FOREIGN KEY (IcaoCode)
+REFERENCES Airline (IcaoCode);
 
 /* Set up the AirportFaaCode Foreign Key */
 ALTER TABLE AirportAirline
 ADD CONSTRAINT  AirportFaaCodeForeignKey
-FOREIGN KEY (AirportFaaCode)
-REFERENCES Airport (AirportFaaCode);
+FOREIGN KEY (FaaCode)
+REFERENCES Airport (FaaCode);
+
+/* Set up the Primary Keys */
+ALTER TABLE AirportAirline
+ADD CONSTRAINT AirportPrimaryKey
+PRIMARY KEY (IcaoCode, FaaCode);
 
 /*******************************************************************************
-Name 
+Aircraft 
 
 Description
 */
-create table Name (
-	Attr 						VARCHAR(256) 		not null,	/* PK */
+create table Aircraft (
+	TailNumber 					VARCHAR(256) 		NOT NULL,	/* PK */
+	Name 						VARCHAR(256),
+	NumberOfPassengers 			Int,
+	Model 						VARCHAR(256),
 
-); /* End of Creation of Name Table */
+); /* End of Creation of Aircraft Table*/
+/* Set up the Primary Keys */
+ALTER TABLE Aircraft
+ADD CONSTRAINT AircraftPrimaryKey
+PRIMARY KEY (TailNumber);
+
+
+/*******************************************************************************
+Flight 
+
+Description
+*/
+create table Flight (
+	FlightId 					Int 				NOT NULL,	/* PK */
+
+); /* End of Creation of Flight Table*/
+/* Set up the Primary Keys */
+ALTER TABLE Flight
+ADD CONSTRAINT FlightPrimaryKey
+PRIMARY KEY ();
+
+
+
 
 /*
 	THIS IS THE TEMPLATE THIS IS THE TEMPLATE THIS IS THE TEMPLATE
